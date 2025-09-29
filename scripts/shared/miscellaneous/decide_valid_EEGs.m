4d0421919b18ea617b69c94b3583e74c3b16f780
FedeC3N <federami@ucm.es>
2025-09-29 17:10:42 +0200

    Select EEGs with valid PLV and power:    It has the first visit    It has at least one more visit

--- scripts/shared/miscellaneous/decide_valid_EEGs.m
+++ scripts/shared/miscellaneous/decide_valid_EEGs.m
@@ -17,14 +17,18 @@ addpath(fullfile('..','io'))
 
 % Load the configuration
 config = init();
-config.measure = 'plv';
 
 % Load the data
 % Read the Excel file
-dummy_filename = sprintf('%s/SEPNECA_data.xlsx',config.path.demographic);
+dummy_filename = '../../../excel/SEPNECA_data.xlsx';
 excel = readcell(dummy_filename,'Sheet','all');
 excel = excel(2:end,:);
 subjects = excel(:,1);
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%% PLV
+config.measure = 'plv';
 
 % For each subject, find and load the plv.
 measure_all = nan(7875,numel(subjects),numel(config.visits),numel(config.bands));
@@ -33,8 +37,8 @@ for isubject = 1 : numel(subjects)
     % Load the file
     current_subject = subjects{isubject};
     current_subject = current_subject(1:5);
-    current_file = sprintf('%s-%s.mat',current_subject,config.measure);
-    current_file = fullfile(config.path.(config.measure),current_file);
+    current_file = sprintf('../../../data/%s/%s-%s.mat',...
+        config.measure, current_subject,config.measure);
     if ~exist(current_file)
         continue
     end
@@ -53,24 +57,76 @@ weird_values_index = find(measure_all > 1);
 measure_all(weird_values_index) = nan;
 
 % Save the valid index to copy in the Excel file
-valid_index = zeros(numel(subjects),1);
+plv_valid_index = zeros(numel(subjects),1);
 
 % Find bad visit 1
-valid_visit_1 = measure_all(:,:,1,4);
-valid_visit_1 = ~isnan(valid_visit_1);
-valid_visit_1 = sum(valid_visit_1,1);
+dummy = nanmean(measure_all,4);
+valid_visit_1 = dummy(:,:,1);
+valid_visit_1 = nansum(valid_visit_1,1);
 valid_visit_1 = valid_visit_1 > 0;
 
 % Any visit valid
-valid_all_visits = measure_all(:,:,2:end,4);
-valid_all_visits = ~isnan(valid_all_visits);
-valid_all_visits = sum(valid_all_visits,3);
-valid_all_visits = sum(valid_all_visits,1);
-valid_all_visits = valid_all_visits > 0;
+dummy = nanmean(measure_all,4);
+valid_rest_visits = dummy(:,:,2:end);
+valid_rest_visits = nansum(valid_rest_visits,3);
+valid_rest_visits = nansum(valid_rest_visits,1);
+valid_rest_visits = valid_rest_visits > 0;
 
 % Valid index
-valid_index = valid_visit_1 & valid_all_visits;
-valid_index = int16(valid_index');
+plv_valid_index = valid_visit_1 & valid_rest_visits;
+plv_valid_index = int16(plv_valid_index');
 
 %%%%
 % COPY valid_index TO THE EXCEL
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%% POW
+config.measure = 'pow';
+
+% For each subject, find and load the plv.
+measure_all = nan(126,172,numel(subjects),numel(config.visits));
+for isubject = 1 : numel(subjects)
+    
+    % Load the file
+    current_subject = subjects{isubject};
+    current_subject = current_subject(1:5);
+    current_file = sprintf('../../../data/%s/%s-%s.mat',...
+        config.measure, current_subject,config.measure);
+    if ~exist(current_file)
+        continue
+    end
+    current_measures = load(current_file);
+    
+    % Store
+    if ~isempty(current_measures.pow.('v1'))
+        measure_all(:,:,isubject,1) = current_measures.pow.('v1');
+    end
+    if ~isempty(current_measures.pow.('v2'))
+        measure_all(:,:,isubject,2) = current_measures.pow.('v2');
+    end
+    if ~isempty(current_measures.pow.('v3'))
+        measure_all(:,:,isubject,3) = current_measures.pow.('v3');
+    end
+    if ~isempty(current_measures.pow.('v4'))
+        measure_all(:,:,isubject,4) = current_measures.pow.('v4');
+    end
+    
+
+end
+
+% Save the valid index to copy in the Excel file
+pow_valid_index = zeros(numel(subjects),1);
+
+% Find bad visit 1
+dummy = measure_all(:,:,:,1);
+valid_visit_1 = squeeze(nanmean(nanmean(dummy,1),2));
+valid_visit_1 = valid_visit_1 > 0;
+
+% Any visit valid
+dummy = nanmean(measure_all(:,:,:,2:4),4);
+valid_rest_visits = squeeze(nanmean(nanmean(dummy,1),2));
+valid_rest_visits = valid_rest_visits > 0;
+
+% Valid index
+pow_valid_index = valid_visit_1 & valid_rest_visits;
+pow_valid_index = int16(pow_valid_index);
