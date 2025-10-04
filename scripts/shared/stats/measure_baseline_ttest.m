848b66414fc9ac84d589cde93719815015451284
FedeC3N <federami@ucm.es>
2025-10-04 18:45:27 +0200

    Measure strength

--- scripts/shared/stats/measure_baseline_ttest.m
+++ scripts/shared/stats/measure_baseline_ttest.m
@@ -15,10 +15,13 @@ switch config.measure
         plv_baseline_ttest(config,measure,group);
     case 'pow'
         pow_baseline_ttest(config,measure,group);
+    case 'strength'
+        strength_baseline_ttest(config,measure,group);
 end
 
 
 end
+
 
 function plv_baseline_ttest(config,measure,group)
 
@@ -62,7 +65,6 @@ end
 
 function pow_baseline_ttest(config,measure,group)
 
-
 % Save space for the results
 p_original = nan(size(measure.all,1),numel(config.bands));
 tstat = nan(size(measure.all,1),numel(config.bands));
@@ -101,7 +103,45 @@ outfile = fullfile(config.path.stats,outname);
 save(outfile,'-struct','output')
 
 
+end
 
+
+function strength_baseline_ttest(config,measure,group)
+
+% Save space for the results
+p_original = nan(size(measure.all,1),numel(config.bands));
+tstat = nan(size(measure.all,1),numel(config.bands));
+
+% Estimate the original p-values for each channel and band
+fprintf(1,'   Estimate original p-values.\n')
+for iband = 1 : numel(config.bands)
+    
+    for isensor = 1 : size(measure.all,1)
+        
+        % Create vectors for ANOVAN model
+        measure_v1 = measure.baseline(isensor,:,iband)';
+        
+        [~,p,CI,stats] = ttest2(measure_v1(group==0),measure_v1(group==1));
+        
+        % Save the results
+        p_original(isensor, iband) = p;
+        tstat(isensor,iband) = stats.tstat;
+        
+    end
+    
+end
+
+% Create the output
+output = [];
+output.test = sprintf('%s BASELINE ttest', config.measure);
+output.dimensions = 'links x bands';
+output.p_original = p_original;
+output.tstat = tstat;
+
+% Save
+outname = sprintf('%s_baseline_ttest_%i_groups_stats.mat', config.measure, numel(config.groups_selected));
+outfile = fullfile(config.path.stats,outname);
+save(outfile,'-struct','output')
 
 
 end
